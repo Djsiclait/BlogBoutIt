4413adc146b4385fa873c3e5246ed67dd05c8592
Eduardo Veras <eduardo.storm@gmail.com>
2016-05-31 19:16:34 -0400

    Dividing code in fuctions and adding H2 database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,14 @@
  * Created by Siclait on 30/05/2016.
  */
 import spark.ModelAndView;
-import spark.Spark;
+import spark.Spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,9 +17,21 @@ import static spark.Spark.*;
 
 public class Main {
 
-    public static void main(String[] args){
+
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_CONNECTION = "jdbc:h2:~/test";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASSWORD = "";
+    private static final String TABLE_NAME= " estudiantes ";
+
+    public static void main(String[] args) throws Exception{
+
 
         staticFileLocation("/public");
+        Class.forName(DB_DRIVER);
+        Connection conn = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+        createBasicTable(conn);
+        createMainPage(conn);
 
         // Initiate server connection
         //DatabaseManager.StartServer();
@@ -22,6 +39,27 @@ public class Main {
         // Terminate server connection
         //DatabaseManager.CloseServer();
 
+    }
+
+
+    public static void createBasicTable(Connection conn) throws SQLException {
+        System.out.println("Creating table in given database...");
+        Statement stmt = conn.createStatement();
+
+        String sql = "CREATE TABLE IF NOT EXISTS "+TABLE_NAME+"( " +
+                "matricula INTEGER NOT NULL, " +
+                "nombre varchar(100) NOT NULL,  " +
+                "apellidos varchar(100) NOT NULL, " +
+                "telefono varchar(50) NOT NULL, " +
+                "PRIMARY KEY (matricula))";
+
+        stmt.executeUpdate(sql);
+        System.out.println("Created table in given database...");
+    }
+
+
+    public static void createMainPage(Connection conn)
+    {
         get("/", (req, res) -> {
             res.status(200);
             Map<String, Object> attributes = new HashMap<>();
@@ -32,4 +70,7 @@ public class Main {
         }, new FreeMarkerEngine());
 
     }
+
+
+
 }
