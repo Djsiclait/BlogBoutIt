1b5513e740414f0328dd0c24b55d7333a088da66
Djsiclait <djidjellypas92@gmail.com>
2016-05-30 23:22:44 -0400

    Add Sparkjava framework: Successful!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,17 @@
 /**
  * Created by Siclait on 30/05/2016.
  */
+import static spark.Spark.*;
+
 public class Main {
 
     public static void main(String[] args){
         // Initiate server connection
-        DatabaseManager.StartServer();
+        //DatabaseManager.StartServer();
 
         // Terminate server connection
-        DatabaseManager.CloseServer();
+        //DatabaseManager.CloseServer();
+
+        get("/", (req, res) -> "Home page");
     }
 }
