a5abae4c4d33265b6f8fa9f7ea00ed2c6e4c139c
djsiclait <djidjellypas92@gmail.com>
2016-06-03 13:14:08 -0400

    Clean code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 /**
  * Created by Siclait on 30/05/2016.
  */
-import java.sql.ResultSet;
-import java.util.ArrayList;
-
 import static spark.Spark.*;
 
 public class Main {
@@ -13,24 +10,6 @@ public class Main {
         staticFileLocation("/public");
 
         PageCreator pages = new PageCreator();
-        // Initiate server connection
-        //DatabaseManager.StartServer();
-        //java.util.Date utilDate = new java.util.Date();
-        //java.sql.Date time = new java.sql.Date(utilDate.getTime());
-        // Terminate server connection
-        //DatabaseManager.CloseServer();
-
-        Tag tag = new Tag(4, "#Swag");
-        Article article = new Article(0);
-
-        pages.DBmanager.ProcessTagsOnArticlea(tag, article);
-
-        ResultSet rs = pages.DBmanager.getConn().createStatement().executeQuery("SELECT * FROM HASHTAG WHERE ARTICULO=" +
-                article.getId());
-
-        while(rs.next())
-            System.out.println(rs.getInt("etiqueta") + " " + rs.getInt("articulo"));
-
     }
 
     static int getHerokuAssignedPort() {
