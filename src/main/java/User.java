5b565df141b50179a423a4399ef3abc460cb175f
djsiclait <djidjellypas92@gmail.com>
2016-06-07 12:30:13 -0400

    Modify User to POJO Entity

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,8 +3,10 @@
  */
 import javax.persistence.*;
 
+@Entity
 public class User {
     // Attributes
+    @Id
     private String username;
     private String name;
     private String password;
@@ -13,41 +15,62 @@ public class User {
 
     // Constructors
     public User(String username) {
-        this.username = username;
+        this.setUsername(username);
     }
 
     public User(String username, String password) {
-        this.username = username;
-        this.password = password;
+        this.setUsername(username);
+        this.setPassword(password);
     }
 
     public User(String username, String name, String password, boolean admin, boolean author){
 
-        this.username = username;
-        this.name = name;
-        this.password = password;
-        this.admin = admin;
-        this.author = author;
+        this.setUsername(username);
+        this.setName(name);
+        this.setPassword(password);
+        this.setAdmin(admin);
+        this.setAuthor(author);
     }
 
-    // Getters
+    // Getters and Setters
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public boolean isAdmin() {
         return admin;
     }
 
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
     public boolean isAuthor() {
         return author;
     }
+
+    public void setAuthor(boolean author) {
+        this.author = author;
+    }
+
 }
