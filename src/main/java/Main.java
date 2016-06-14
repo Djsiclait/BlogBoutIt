ff44ae55307dcc9b743d355f9195d06ba5c47e40
Eduardo Veras <eduardo.storm@gmail.com>
2016-06-14 11:25:08 -0400

    Set correct port to heroku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,21 +8,17 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        getHerokuAssignedPort();
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
         PageCreator pages = new PageCreator();
     }
 
-    static void getHerokuAssignedPort() {
-        ProcessBuilder process = new ProcessBuilder();
-        Integer port;
-        if (process.environment().get("PORT") != null) {
-            port = Integer.parseInt(process.environment().get("PORT"));
-        } else {
-            port = 4567;
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-
-        setPort(port);
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
